8882b78d40a15fde69bb52657dbe29ff46285258
Pedro Capaça <pedro.capaca@gmail.com>
2009-11-23 22:02:41 -0200

    Removidos os arquivos de backuo

--- bandmanager/app/helpers/application_helper.rb
+++ bandmanager/app/helpers/application_helper.rb
@@ -1,3 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def verify_current_page(menu_item)
+    if controller_name == menu_item
+      "current_page_item"
+    end
+  end
+
+  def corner(*ids)
+    ids.each do |id|
+      output << ""
+    end
+  end
+
 end
