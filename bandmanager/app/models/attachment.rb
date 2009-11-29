bc85485fddd708055be8d5b11e06d1fedbcc17f4
Pedro <pedro@pedro-desktop.(none)>
2009-11-28 23:38:18 -0200

    Adicionado o suporte a upload de imagens e thumbnails em banda. Havia esquecido de adicionar or arquivos

---
+++ bandmanager/app/models/attachment.rb
@@ -0,0 +1,9 @@
+class Attachment < ActiveRecord::Base
+  has_attachment :content_type => :image, 
+                 :storage => :file_system, 
+                 # :max_size => 500.kilobytes,
+                 :resize_to => '300x300>',
+                 :thumbnails => { :thumb => '100x100>' }
+
+  validates_as_attachment
+end
