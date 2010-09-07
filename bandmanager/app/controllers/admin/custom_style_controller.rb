9c02230a4bb8ded0e988bbbd0410176c1d7517c1
Pedro Capaça <pedro.capaca@gmail.com>
2010-09-06 21:27:21 -0300

    Trabalhando no preview de layout. Falta testar e habilitar a customização na tela inicial do site

--- bandmanager/app/controllers/admin/custom_style_controller.rb
+++ bandmanager/app/controllers/admin/custom_style_controller.rb
@@ -8,11 +8,16 @@ class Admin::CustomStyleController < Admin::BaseController
     @band = Band.find params[:id]
     @band.custom_style = params[:custom_style]
     render  :template => 'site/index', 
-            :layout => 'site', 
+            :layout => 'site_preview', 
             :status => :error
   end
   
   def update
+    @band = Band.find params[:id]
+    @band.custom_style = params[:custom_style]
+    
+    @band.save
+    redirect_to "/admin/custom_style/edit/#{@band.id}"
   end
   
   private
